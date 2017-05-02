0b12937cb88ac47ebd11e061f712b139a1e330fb
jlewiskilgore <jlewis.kilgore@gmail.com>
2017-05-02 01:01:50 +0000

    Get date query parameter from URL of GET request

--- server.js
+++ server.js
@@ -2,9 +2,28 @@ var express = require('express')
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    res.send('Timestamp Microservice API');
+});
+
+app.get('/:date', function(req, res) {
+    var naturalDate = null;
+    var unixDate = null;
+    
+    var userDate = req.params.date;
+    
+    if(Number(userDate)) {
+        unixDate = userDate;
+    }
+    else if(new Date(userDate)) {
+        naturalDate = userDate;
+    }
+    
+    var result = {"unixTimestamp": unixDate, 
+        "naturalLanguageTimestamp": naturalDate };
+   
+    res.json(result);
 });
 
 app.listen(8080, function() {
-    console.log("Hello World Running on Port 8080");
+    console.log("Server Listening on Port 8080");
 });
