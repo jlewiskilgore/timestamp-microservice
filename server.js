1ccefc461d6c30a2c07eae816b49e30d777f45ba
jlewiskilgore <jlewis.kilgore@gmail.com>
2017-05-03 01:51:58 +0000

    Add instructions and sample output to home route

--- server.js
+++ server.js
@@ -2,7 +2,24 @@ var express = require('express')
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Timestamp Microservice API');
+    res.send(
+        '<h1>Timestamp Microservice API</h1>'+
+        '<br>'+
+        '<code>https://timestamp-microservice-jlewiskilgore.c9users.io/[datestring]</code>'+
+        '<br>'+
+        '<br>'+
+        '<h3>Example Useage: </h3>'+
+        '<br>'+
+        '<code>https://timestamp-microservice-jlewiskilgore.c9users.io/July%2022,%201988</code>'+
+        '<br>'+
+        '<br>'+
+        '<code>https://timestamp-microservice-jlewiskilgore.c9users.io/585532800</code>'+
+        '<br>'+
+        '<br>'+
+        '<h3>Sample Output: </h3>'+
+        '<br>'+
+        '{"unixTimestamp": 585532800,"naturalLanguageTimestamp":"July 22, 1988"}'
+    );
 });
 
 app.get('/:date', function(req, res) {
